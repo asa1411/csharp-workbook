f91ae0ad56cf16273a6794daadfafc31d7033fc3
asa1411 <jungil_cho@yahoo.com>
2019-04-02 20:36:00 -0500

    start

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -5,7 +5,14 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
-        {
+        {// part1-1
+
+
+        // part-2
+
+        // part-3
+
+        
             Console.WriteLine("Hello World!");
         }
     }
