e9098f2320f2bb4bde52e1ccd6127e3e6721167a
asa1411 <jungil_cho@yahoo.com>
2019-04-04 18:48:48 -0500

    greetings

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + input);
         }
     }
 }
