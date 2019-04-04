c887347e2ae314414394dc5d70fe214e796d665b
asa1411 <jungil_cho@yahoo.com>
2019-04-04 18:39:05 -0500

    greeting coding

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -5,15 +5,10 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
-        {// part1-1
-
-
-        // part-2
-
-        // part-3
-
-        
-            Console.WriteLine("Hello World!");
+        {// write a program that greets you and ask for your name
+        Console.WriteLine("Hello, What is your name?");
+        String input = Console.ReadLine();
+        Console.WriteLine("Nice to meet you " + input);
         }
     }
 }
