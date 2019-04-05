45fbb46f34dfdd10fa913c0f09507885f60c4797
asa1411 <jungil_cho@yahoo.com>
2019-04-04 20:23:11 -0500

    class practice

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,12 +4,26 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Hello(string[] args)
+        {   //print out a greeting to the screen
             Console.WriteLine("Hello World!");
+            //ask the user for their name
             Console.WriteLine("What is your name?");
+            //read the name they type in into a String variable
             string input = Console.ReadLine();
             Console.WriteLine("Nice to meet you " + input);
+        }
+        static void Main(string[] args)
+        {   //given a dollar amount
+        int amount = 350;
+        Console.WriteLine(amount);
+        int x100;
+        int x20;
+        int x10;
+        int x5;
+        int newAmount = amount 
+
+
         }
     }
 }
