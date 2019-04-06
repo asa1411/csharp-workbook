47d8af8943e396fd4655aa7a286b7c5eef36dd34
asa1411 <jungil_cho@yahoo.com>
2019-04-06 14:19:04 -0500

    extra

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,6 +7,14 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
+            //Enter a word in to a console
+            Console.WriteLine("Let's make a pigLatin. Please enter a word.");
+            //Read the word
+            string input = Console.ReadLine();
+            //make a string variable to signify vowels
+            string [] vowels = {"a","e", "i", "o", "u", "A", "E", "I", "O", "U"};
+            
+            
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
