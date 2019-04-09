5d2881e21c3b90684942000d6dc64f1319cfe423
asa1411 <jungil_cho@yahoo.com>
2019-04-09 10:14:08 -0500

    complete week1-2

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,33 +4,7 @@ namespace HelloWorld
 {
     class Program
     {
-        static void practice(string[] args)
-        {
-            string example = "prtyAnother is Lowercase.";
-            //convert any capital letters to lowercase
-            string exampleLower = example.ToLower();
-            //find the index of a certain character in a string
-            int num = exampleLower.IndexOf('l');
-            //write to console the string variable exampleLower
-            Console.WriteLine(num);
-            //write a char variable with an array of vowels
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            //write to console the index of array for character e
-            Console.WriteLine(vowels[1]);
-            //use substring method, to retrieve substring
-            string part = exampleLower.Substring(5);
-            Console.WriteLine(part);
-            //use IndexofAny method to index of a first occurrance of a character in an array of characters
-            int num1 = exampleLower.IndexOfAny(vowels);
-            //write the index number 
-            Console.WriteLine(num1);
-            //split a string into two parts. Declare str1 as part of the string starting from index 0 to index of (num1 -1) which is the number of characters in the string before a vowel is found
-            string str1 = exampleLower.Substring(0, num1 - 1);
-            //declare str2 as the rest of the string after the first part is removed
-            string str2 = exampleLower.Substring(num1);
-            Console.WriteLine(str1);
-            Console.WriteLine(str2);
-        }
+
         static void Main(string[] args)
         {   //Write to console two sentences
             Console.WriteLine("Let's play a pigLatingame.");
@@ -43,21 +17,29 @@ namespace HelloWorld
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
             //decare int variable index to be set to index number of first occurrence of any vowel in the string imputLowercase
             int index = inputLowercase.IndexOfAny(vowels);
+            //declare int variable indexLast to be set to the value of last occurrence of any vowel in the string imputLowercase
+            int indexLast = inputLowercase.LastIndexOfAny(vowels);
+
             //declare string variable beforeIndex to be set to the substring which starts from a letter of index 0 and continue to as many letters as the number of index
             string beforeIndex = inputLowercase.Substring(0, index);
             //declare string variable afterIndex to be set to the substring which starts from a letter of index number and continue to the end of the string
             string afterIndex = inputLowercase.Substring(index);
             string space = " ";
-            //declare lastCharacter to set to the last letter of the input string
-            char lastCharacter = inputLowercase[inputLowercase.Length - 1];
-            //declare firstCharacter to set to the first letter of the input string
-            char firstCharacter = inputLowercase[0];
-            if ((firstCharacter) && (lastCharacter === vowels))
-
-
-
+            //if statement about vowels come first and last of the string
+            if (index == 0 && indexLast == inputLowercase.Length - 1)
+            {//then add yay to the word 
+                Console.WriteLine("Your pigLatin is" + space + inputLowercase + "yay");
+            }//if vowel comes first but the word does not end up with a vowel
+            else if (index == 0 && indexLast != inputLowercase.Length - 1)
+            {//then add ay to the word
+                Console.WriteLine("Your pigLatin is" + space + inputLowercase + "ay");
+            }
+            else
+            {//if the first letter of the word is not a vowel, then the segment before the first vowel goes back to the the segment after the first vowel and add ay to the switched word
                 Console.WriteLine("Your pigLatin is" + space + afterIndex + beforeIndex + "ay");
-            Console.WriteLine(firstCharacter + space + lastCharacter);
+            }
+
+
         }
     }
 }
