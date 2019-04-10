365bf954c7783551d3c306375ba6d00d821d29ab
asa1411 <jungil_cho@yahoo.com>
2019-04-09 19:03:37 -0500

    new branch

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,23 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input > 0)
+            {
+
+                Console.WriteLine("positive");
+
+            }
+            else if (input < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("zero");
+            }
         }
     }
 }
