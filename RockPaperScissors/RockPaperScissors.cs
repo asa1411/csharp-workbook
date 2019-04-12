8d3e0830bbf1a8cffe13bf7152cdf409e1fba256
asa1411 <jungil_cho@yahoo.com>
2019-04-11 22:02:30 -0500

    first step

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -15,10 +15,42 @@ namespace RockPaperScissors
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
-        
+
         public static string CompareHands(string hand1, string hand2)
         {
             // Your code here
+            if (hand1 == hand2)
+            {
+                return "It's a tie!";
+            }
+
+            if (hand1 == "rock")
+            {
+                if (hand2 == "scissors")
+                {
+                    return "Hand one wins!";
+                }
+                // If we reach here, player 2 must have dealt paper
+                return "Hand two wins!";
+            }
+
+            if (hand1 == "paper")
+            {
+                if (hand2 == "rock")
+                {
+                    return "Hand one wins!";
+                }
+                return "Hand two wins!";
+            }
+
+            if (hand1 == "scissors")
+            {
+                if (hand2 == "paper")
+                {
+                    return "Hand one wins!";
+                }
+                return "Hand two wins!";
+            }
             return hand1 + ' ' + hand2;
         }
     }
