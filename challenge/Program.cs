7f9613399dbc837af9da57685be85a5ad8bd8739
asa1411 <jungil_cho@yahoo.com>
2019-04-23 21:06:17 -0500

    challenge acctivity

---
+++ challenge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Write a program that asks the user for 5 integers. 
+         //Handle the user giving you non-integer input.
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter a number.");
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    sum = sum + number;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong.");
+                    Console.WriteLine("the message from the exception is " + e.Message);
+
+                }
+                finally
+                {
+                    Console.WriteLine("Thank you.");
+                }
+
+                Console.WriteLine("The sum is " + sum);
+            }
+
+
+
+        }
+    }
+}
