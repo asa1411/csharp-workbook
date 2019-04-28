4bd6194ab1660d6aebc4b3f579b983a72479eb74
asa1411 <jungil_cho@yahoo.com>
2019-04-27 20:20:33 -0500

    all files

---
+++ LoopQuiz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LoopQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] ar = { 10, 2, 3, 4, 50 };
+            int biggestOneSoFar = ar[0];
+            for (int i = 1; i < ar.Length; i++)
+            {
+                int currentNumber = ar[i];
+                if (currentNumber > biggestOneSoFar)
+                    biggestOneSoFar = currentNumber;
+
+                Console.WriteLine("Iteration #" + i + " =>" + biggestOneSoFar);
+            }
+
+        }
+    }
+
+
+}
