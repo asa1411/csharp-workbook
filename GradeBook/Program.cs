1dce038c521692e2a7d2ed50fbb97622431d3880
asa1411 <jungil_cho@yahoo.com>
2019-04-28 18:45:41 -0500

    gradebook assignment complete

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -9,7 +9,7 @@ namespace GradeBook
         static void Main(string[] args)
         {//1. Your program should ask the user for a set of 5 students and their grades
          //2. It should print the students' name and some statistics about their grades (the grades, min, max, and/or average of the grades and the student name)
-         //Umm...many other files in the PR...
+
 
             Dictionary<string, string[]> gradebook = new Dictionary<string, string[]>();
 
