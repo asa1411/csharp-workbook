3d660c3f3cdeed0d197c407541f277d4fee6cf56
asa1411 <jungil_cho@yahoo.com>
2019-04-28 18:23:07 -0500

    complete gradebook

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -11,52 +11,44 @@ namespace GradeBook
          //2. It should print the students' name and some statistics about their grades (the grades, min, max, and/or average of the grades and the student name)
 
             Dictionary<string, string[]> gradebook = new Dictionary<string, string[]>();
-            // grades.Add("names", new string[] { "23", "45", "100" });
 
-            for (int i = 0; i < 2; i++)
+
+            for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("What is your name?");
                 string names = Console.ReadLine();
                 Console.WriteLine("What are your recent three scores? Separate scores by comma");
                 string[] grades = Console.ReadLine().Split(',');
                 gradebook.Add(names, grades);
-
             }
 
             foreach (string names in gradebook.Keys)
             {
-                Console.WriteLine("Key: " + names);
+                Console.Write("Name: " + names + "; Grades: ");
+
                 string[] grades = gradebook[names];
                 int g = int.Parse(grades[0]);
                 int Min = g;
                 int Max = g;
                 int Sum = 0;
 
-                foreach (var gr in gradebook[names])
+                for (int i = 0; i < grades.Length; i++)
                 {
-                    Console.WriteLine(gr);
-                    // Console.WriteLine(grades);
-
-                    for (int i = 0; i < grades.Length; i++)
+                    Sum = Sum + Convert.ToInt32(grades[i]);
+                    if (Min >= Convert.ToInt32(grades[i]))
                     {
-                        Sum = Sum + Convert.ToInt32(grades[i]);
-                        if (Min >= Convert.ToInt32(grades[i]))
-                        {
-                            Min = Convert.ToInt32(grades[i]);
-                        }
-                        if (Max <= Convert.ToInt32(grades[i]))
-                        {
-                            Max = Convert.ToInt32(grades[i]);
-                        }
-
+                        Min = Convert.ToInt32(grades[i]);
                     }
-
-
+                    if (Max <= Convert.ToInt32(grades[i]))
+                    {
+                        Max = Convert.ToInt32(grades[i]);
+                    }
+                    Console.Write(grades[i] + " ");
                 }
+                Console.WriteLine(" ");
+                Console.WriteLine("avg: " + Sum / grades.Length + "; max: " + Max + "; min:" + Min);
 
 
-                Console.WriteLine("The maximum number is " + Max + "and the min number is " + Min);
-                Console.WriteLine("The sum of the grades is " + Sum);
             }
 
         }
