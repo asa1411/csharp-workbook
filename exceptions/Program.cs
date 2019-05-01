2ddeed6db9c34daea0cd71e5df71f90af3c3623f
asa1411 <jungil_cho@yahoo.com>
2019-04-30 23:06:56 -0500

    exception assignment

--- exceptions/Program.cs
+++ exceptions/Program.cs
@@ -8,19 +8,14 @@ namespace RockPaperScissors
         public static int ComputerScore;
         public static int PlayerScore;
         public static int TieScore;
-        // List<string> rps = new List<string>();
 
         public static void Main()
         {
-            // Handone type = new Handone();
             try
             {
-
                 Console.WriteLine("Enter hand 1:");
                 string hand1 = Console.ReadLine().ToLower();
                 //Generate random number under a name "randomHand"
-
-
                 Random hand2Generator = new Random();
                 int randomHand = hand2Generator.Next(0, 3);
                 //Assign string hand2 to rock, paper, or scissors according to the if conditions 
@@ -39,35 +34,18 @@ namespace RockPaperScissors
                     hand2 = "paper";
 
                 }
+
                 Console.WriteLine("The hand of play2 is " + hand2);
                 //execute CompareHands method with strings hand1 and hand2
                 Console.WriteLine(CompareHands(hand1, hand2));
-                // Console.WriteLine(Handone(hand1));
-
-
-
                 //write the hand of computer play
-
                 //Write the results of incremental numbers for hand1, hand2 and draw
                 Console.WriteLine("Hand1: " + PlayerScore + " wins" + ", Hand2: " + ComputerScore + " wins" + "," + " Ties: " + TieScore);
-                //execute replay method
                 Replay();
-                // type.showType();
-                // leave this command at the end so your program does not close automatically
-
             }
             catch (Exception)
             {
                 Console.WriteLine("You typed wrong.");
-            }
-            // catch (TypeException e)
-            // {
-            //     Console.WriteLine("TypingException: {0}", e.Message);
-            // }
-
-            finally
-            {
-                Console.WriteLine("End of play");
             }
         }
 
@@ -84,26 +62,7 @@ namespace RockPaperScissors
                 Console.WriteLine("Thank you for playing.");
             }
         }
-    //     public class Handone
-    //     {List<string> rps = new List<string>();
-    //         rps={"rock", "paper", "scissors"};
-    //         public void showType() {
-    //     if(rps.Contains(hand1)) {
-    //      throw (new TypeException("Typing error found"));
-    //   } else {
-    //      Console.WriteLine("Typing error: {0}");
-    //   }
 
-    //     }
-    //     }
-    //     public class TypeException : Exception
-    //     {
-
-    //         public TypeException(string message) : base(message)
-    //         {
-
-    //         }
-    //     }
         public static string CompareHands(string hand1, string hand2)
         {
             List<string> rps = new List<string>();
@@ -157,11 +116,9 @@ namespace RockPaperScissors
             }
             else
             {
-                throw new Exception();
+                throw new Exception();//throw an exception when one of the rock, paper and scissors are not entered.
             }
-            // int g = 3;
-            // int h = 0;
-            // Console.WriteLine(g / h);
+
             return hand1 + ' ' + hand2;
         }
     }
