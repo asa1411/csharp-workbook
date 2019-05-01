3a75c8f95fdb97d020a806bce64154cace44fecf
asa1411 <jungil_cho@yahoo.com>
2019-04-30 19:23:00 -0500

    on

---
+++ ClassPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class P
+    {
+        public Char gender;
+        public string name;
+        public Person(bool x)
+        {
+            if (x)
+            {
+                this.name = "Jane Dee";
+                this.gender = 'F';
+            }
+            else
+            {
+                this.name = "John Doe";
+                this.gender = 'M';
+            }
+        }
+        
+    }
+}
