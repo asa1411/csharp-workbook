fcc5f329d8db4b855f8873107bb38342514e6d64
asa1411 <jungil_cho@yahoo.com>
2019-05-31 22:16:30 -0500

    books framework by instructor

---
+++ toDoList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace toDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dao dao = new Dao();
+            dao.create("Cat in the Hat", Type.Kids);
+            dao.create("The Hobbit", Type.Fiction);
+            
+            foreach(Book b in dao.listBooks())
+            {
+                Console.WriteLine(b.title+" is a "+b.type+" book");
+            }
+        }
+    }
+    
+}
