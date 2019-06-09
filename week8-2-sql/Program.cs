d535e3857d1e1a80f81d2ec6de23cae317a6fba5
asa1411 <jungil_cho@yahoo.com>
2019-06-09 01:00:26 -0500

    text file of sqlite3

---
+++ week8-2-sql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week8_2_sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
