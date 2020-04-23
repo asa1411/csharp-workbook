026147fc615468e61ec50260183e353fc123b13f
asa1411 <jungil_cho@yahoo.com>
2020-04-22 22:13:59 -0500

    sum loop

---
+++ practice1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number or 'ok' to exit.");
+            String input = Console.ReadLine();
+            int sum = 0;
+            while (input != "ok")
+            {
+                Console.WriteLine("Enter a number or 'ok' to exit.");
+                String input1 = Console.ReadLine();
+                int input1Number = int.Parse(input1);
+                sum = sum + input1Number;
+                Console.WriteLine(sum);
+
+            }
+
+        }
+    }
+}
