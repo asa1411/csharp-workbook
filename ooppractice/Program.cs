bf534b5a4a3b21812d6603c87852e644030b5119
asa1411 <jungil_cho@yahoo.com>
2020-06-02 01:27:15 -0500

    ooppractice-ship

--- ooppractice/Program.cs
+++ ooppractice/Program.cs
@@ -4,36 +4,105 @@ namespace ooppractice
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
-            trees loquat = new trees("decidious", "yellow");
-            loquat.height = 12;
-            loquat.evergreen = true;
-            
-            trees figtree = new trees("decidious", "summer sweet");
-            figtree.height = 10;
-            figtree.evergreen = false;
-            
-            Console.WriteLine($"Figtree's leaves are {figtree.leaves}, and its fruit is {figtree.fruit}.");
-            Console.WriteLine("Is loquat evergreen? Yes, it is {0}.", loquat.evergreen);
-            Console.WriteLine($"My figtree is {figtree.height} feet high, and my loquat is {loquat.height} feet high.");
-
+            Person leia = new Person("Leia", "Organa", "Rebel");
+            Person darth = new Person("Darth", "Vader", "Imperial");
+            Ship falcon = new Ship("Rebel", "Smuggling", 2);
+            Ship tie = new Ship("Tie", "Fighter", 1);
+            Station space = new Station("Rebel", "OK");
+            Station star = new Station("Death", "Never");
+            Console.WriteLine("Hello world!");
         }
     }
 
-    class trees
+    class Person
     {
-        public string leaves;
-        public string fruit;
-        public bool evergreen;
-        public int height;
-
-        public trees (string initialleaves, string initialfruit)
+        private string firstName;
+        private string lastName;
+        private string alliance;
+        public Person(string firstName, string lastName, string alliance)
         {
-            this.leaves = initialleaves;
-            this.fruit = initialfruit;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.alliance = alliance;
         }
+
+        public string FullName
+        {
+            get
+            {
+                return this.firstName + " " + this.lastName;
+            }
+
+            set
+            {
+                string[] names = value.Split(' ');
+                this.firstName = names[0];
+                this.lastName = names[1];
+            }
+        }
+    }
+
+    class Ship
+    {
+        private Person[] passengers;
+        public Ship(string alliance, string type, int size)
+        {
+            this.Type = type;
+            this.Alliance = alliance;
+            this.passengers = new Person[size];
+        }
+
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public string Alliance
+        {
+            get;
+            set;
+        }
+
+        public string Passengers
+        {
+            get
+            {
+                foreach (var person in passengers)
+                {
+                    Console.WriteLine(String.Format("{0}", person.FullName));
+                }
+
+                return "That's Everybody!";
+            }
+        }
+
+        public void EnterShip(Person person, int seat)
+        {
+            this.passengers[seat] = person;
+        }
+
+        public void ExitShip(int seat)
+        {
+            this.passengers[seat] = null;
+        }
+
+    }
+    class Station
+    {
+        public string name { get; set; }
+        public string alliance { get; set; }
+        public Station(string initialName, string initialAlliance)
+        {
+            this.name = initialName;
+            this.alliance = initialAlliance;
+        }
+        public void dockShip(Ship ship)
+        {; }
+        public void leaveStation()
+        { }
 
     }
 }
