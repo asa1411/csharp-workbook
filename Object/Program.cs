6b61b33653c7f5c0a1e378d5ca9e9a8ed39e8692
asa1411 <45476911+asa1411@users.noreply.github.com>
2020-10-24 19:12:45 -0500

    object practice

---
+++ Object/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Object
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jungil!");
+           tool hammer = new tool ();
+           tool saw = new tool ();
+           hammer.type = "hometool";
+           hammer.size = "short";
+           saw.type = "hometool";
+           saw.size = "medium";
+            Console.WriteLine ("Hammer is for " + hammer.type + ". Its size is " + hammer.size+".");
+        }
+    }
+
+    public class people
+    {
+        public string nationality;
+        public string color;
+        public int age;
+        public string career;
+
+    }
+    public class tool
+    {
+        public string type;
+        public string size;
+    }
+}
